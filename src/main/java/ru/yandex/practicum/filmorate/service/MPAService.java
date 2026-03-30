fa35225b34125a732136043d702b6c5e6b7c8f6b
Andrey Chaykovskiy <andy.tchaikovsky@yandex.ru>
2026-03-30 01:51:05 -0400

    removed unused imports from mpa service

--- src/main/java/ru/yandex/practicum/filmorate/service/MPAService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MPAService.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.MPA;
 import ru.yandex.practicum.filmorate.storage.mpa.MpaDbStorage;
